5f2bbb2eff3e1af34dbcf20f82760e8a82f0dee1
Ethan3600 <Ethan3600@gmail.com>
2017-03-30 22:17:04 -0400

    adding updated code

--- app/code/local/Ethan/Cronmanager/Helper/Data.php
+++ app/code/local/Ethan/Cronmanager/Helper/Data.php
@@ -1,5 +1,79 @@
 <?php
 class Ethan_Cronmanager_Helper_Data extends Mage_Core_Helper_Abstract
 {
-   
+	public function validateScheduledTime($timeStr)
+	{
+		if ($timeStr === 'queue')
+		{
+			// return the last scheduled cronjob +1 hour
+			$cronJobCollection = Mage::getModel('cron/schedule')->getCollection()
+			->addFieldToFilter('status', array('eq' => Mage_Cron_Model_Schedule::STATUS_PENDING));
+			$endOfQueueTime = $cronJobCollection->orderByScheduledAt()->getLastItem()->getScheduledAt();
+			$endOfQueueTime = !is_null($endOfQueueTime) ? $endOfQueueTime : now();
+			$dateObj = DateTime::createFromFormat('Y-m-d H:i:s', $endOfQueueTime);
+			$dateInterval = new DateInterval('P0Y0M0DT1H0M0S');
+			$dateObj->add($dateInterval);
+			return $dateObj->format('Y-m-d H:i:s');
+				
+		}
+		$timeStr = htmlentities(trim($timeStr), ENT_QUOTES, 'UTF-8');
+		$dateObj = DateTime::createFromFormat('Y-m-d H:i:s', $timeStr);
+		$nowObj = DateTime::createFromFormat('Y-m-d H:i:s', now());
+		if ($dateObj !== false) {
+			//valid time
+			$dateDiff = ($nowObj > $dateObj);
+			if ($dateDiff)
+			{
+				$warning = "Your Cron Job was saved successfully, but it seems to be scheduled in the past.  If this was not your intention, please try again.";
+				return array($timeStr, 'warning' => $warning);
+			}
+			return $timeStr;
+		}
+		else{
+			//invalid time
+			Mage::throwException("Invalid time format");
+		}
+	
+	}
+	
+	public function dispatchCron($cronJobId, $schedule = null)
+	{
+		if (is_null($schedule))
+		{
+			$schedule = Mage::getSingleton('cron/schedule')->load($cronJobId);
+		}
+		$jobsRoot = Mage::getConfig()->getNode('crontab/jobs');
+		$jobConfig = $jobsRoot ->{$schedule->getJobCode()};
+		$isAlways = false;
+		
+		$runConfig = $jobConfig->run;
+		if ($runConfig->model) {
+			if (!preg_match(Mage_Cron_Model_Observer::REGEX_RUN_MODEL, (string)$runConfig->model, $run))
+			{
+				Mage::throwException(Mage::helper('cronmanager')->__('Invalid model/method definition, expecting "model/class::method".'));
+			}
+			if (!($model = Mage::getModel($run[1])) || !method_exists($model, $run[2]))
+			{
+				Mage::throwException(Mage::helper('cronmanager')->__('Invalid callback: %s::%s does not exist', $run[1], $run[2]));
+			}
+			$callback = array($model, $run[2]);
+			$arguments = array($schedule);
+		}
+		if (empty($callback))
+		{
+			Mage::throwException(Mage::helper('cronmanager')->__('No callbacks found'));
+		}
+		
+		$schedule
+		->setExecutedAt(strftime('%Y-%m-%d %H:%M:%S', time()))
+		->save();
+		
+		call_user_func_array($callback, $arguments);
+		
+		$schedule
+		->setStatus(Mage_Cron_Model_Schedule::STATUS_SUCCESS)
+		->setFinishedAt(strftime('%Y-%m-%d %H:%M:%S', time()));
+		return $schedule;
+	}
+	
 }
--- app/code/local/Ethan/Cronmanager/controllers/Adminhtml/Cronmanager/IndexController.php
+++ app/code/local/Ethan/Cronmanager/controllers/Adminhtml/Cronmanager/IndexController.php
@@ -2,8 +2,13 @@
 
 class Ethan_Cronmanager_Adminhtml_Cronmanager_IndexController extends Mage_Adminhtml_Controller_Action
 {
-	protected function _isAllowed(){
-		return true;
+	protected function _isAllowed()
+	{
+		$magentoAccessAllowed = Mage::getSingleton('admin/session')->isAllowed('admin/system/cronmanager');
+		if($magentoAccessAllowed)
+		{
+			return true;
+		}
 	}
 	
 	public function indexAction()
@@ -46,12 +51,13 @@ class Ethan_Cronmanager_Adminhtml_Cronmanager_IndexController extends Mage_Admin
 	public function saveAction()
 	{
 		$params = $this->getRequest()->getParams();
+		$helper = Mage::helper('cronmanager');
 		try 
 		{
 			if(isset($params['id']))
 			{
 				$cronJob = Mage::getModel('cron/schedule')->load($params['id']);
-				$scheduledAt = $this->_validateScheduledTime($params['scheduled_at']);
+				$scheduledAt = $helper->validateScheduledTime($params['scheduled_at']);
 				if (is_array($scheduledAt))
 				{
 					$cronJob->setScheduledAt($scheduledAt[0]);
@@ -64,13 +70,12 @@ class Ethan_Cronmanager_Adminhtml_Cronmanager_IndexController extends Mage_Admin
 				$cronJob->save();
 				$this->_redirect('*/*/index');
 				$this->_getSession()->addSuccess("Your Cron Job was successfully edited.");
-				return;
-				
+				return;		
 			}
 			$cronJob = Mage::getModel('cron/schedule');
 			$cronJob->setStatus(Mage_Cron_Model_Schedule::STATUS_PENDING);
 			$cronJob->setJobCode($params['job_code']);
-			$scheduledAt = $this->_validateScheduledTime($params['scheduled_at']);
+			$scheduledAt = $helper->validateScheduledTime($params['scheduled_at']);
 			$cronJob->setCreatedAt(now());
 			if (is_array($scheduledAt))
 			{
@@ -98,78 +103,56 @@ class Ethan_Cronmanager_Adminhtml_Cronmanager_IndexController extends Mage_Admin
 	
 	public function flushAction()
 	{
-		$history = Mage::getModel('cron/schedule')->getCollection()
-		->addFieldToFilter('status', array('in'=>array(
-				Mage_Cron_Model_Schedule::STATUS_SUCCESS,
-				Mage_Cron_Model_Schedule::STATUS_MISSED,
-				Mage_Cron_Model_Schedule::STATUS_ERROR,
-		)))
-		->load();
-		
-		$historyLifetimes = array(
-				Mage_Cron_Model_Schedule::STATUS_SUCCESS => Mage::getStoreConfig(Mage_Cron_Model_Observer::XML_PATH_HISTORY_SUCCESS)*60,
-				Mage_Cron_Model_Schedule::STATUS_MISSED => Mage::getStoreConfig(Mage_Cron_Model_Observer::XML_PATH_HISTORY_FAILURE)*60,
-				Mage_Cron_Model_Schedule::STATUS_ERROR => Mage::getStoreConfig(Mage_Cron_Model_Observer::XML_PATH_HISTORY_FAILURE)*60,
-		);
-		
-		$now = time();
-		foreach ($history->getIterator() as $record) {
-			if (strtotime($record->getExecutedAt()) < $now-$historyLifetimes[$record->getStatus()]) {
-				$record->delete();
+		try 
+		{
+			$history = Mage::getModel('cron/schedule')->getCollection()
+			->addFieldToFilter('status', array('in'=>array(
+					Mage_Cron_Model_Schedule::STATUS_SUCCESS,
+					Mage_Cron_Model_Schedule::STATUS_MISSED,
+					Mage_Cron_Model_Schedule::STATUS_ERROR,
+			)))
+			->load();
+			
+			$historyLifetimes = array(
+					Mage_Cron_Model_Schedule::STATUS_SUCCESS => Mage::getStoreConfig(Mage_Cron_Model_Observer::XML_PATH_HISTORY_SUCCESS)*60,
+					Mage_Cron_Model_Schedule::STATUS_MISSED => Mage::getStoreConfig(Mage_Cron_Model_Observer::XML_PATH_HISTORY_FAILURE)*60,
+					Mage_Cron_Model_Schedule::STATUS_ERROR => Mage::getStoreConfig(Mage_Cron_Model_Observer::XML_PATH_HISTORY_FAILURE)*60,
+			);
+			
+			$now = time();
+			foreach ($history->getIterator() as $record) {
+				if (strtotime($record->getExecutedAt()) < $now-$historyLifetimes[$record->getStatus()]) {
+					$record->delete();
+				}
 			}
+			
+			// save time history cleanup was ran with no expiration
+			Mage::app()->saveCache(time(), Mage_Cron_Model_Observer::CACHE_KEY_LAST_HISTORY_CLEANUP_AT, array('crontab'), null);
+			$this->_redirect('*/*/index');
+			return $this;
+		}
+		catch (Exception $e)
+		{
+			Mage::throwException("There was an error flushing the cron jobs.  Please check the log for details");
+			Mage::log($e->getMessage(), null, 'cronmanager.log');
 		}
 		
-		// save time history cleanup was ran with no expiration
-		Mage::app()->saveCache(time(), Mage_Cron_Model_Observer::CACHE_KEY_LAST_HISTORY_CLEANUP_AT, array('crontab'), null);
-		$this->_redirect('*/*/index');
-		return $this;
 	}
 
 	public function dispatchAction()
 	{
+		$helper = Mage::helper('cronmanager');
 		$scheduleId = $this->getRequest()->getParam('id');
-		$schedule = Mage::getModel('cron/schedule')->load($scheduleId);
-		$jobsRoot = Mage::getConfig()->getNode('crontab/jobs');
-		$jobConfig = $jobsRoot ->{$schedule->getJobCode()};
-		$isAlways = false;
-		
-		$runConfig = $jobConfig->run;
-		
-		$errorStatus = Mage_Cron_Model_Schedule::STATUS_ERROR;
+		$schedule = Mage::getSingleton('cron/schedule')->load($scheduleId);
 		try 
 		{
-			if ($runConfig->model) {
-				if (!preg_match(Mage_Cron_Model_Observer::REGEX_RUN_MODEL, (string)$runConfig->model, $run)) 
-				{
-					Mage::throwException(Mage::helper('cronmanager')->__('Invalid model/method definition, expecting "model/class::method".'));
-				}
-				if (!($model = Mage::getModel($run[1])) || !method_exists($model, $run[2])) 
-				{
-					Mage::throwException(Mage::helper('cronmanager')->__('Invalid callback: %s::%s does not exist', $run[1], $run[2]));
-				}
-				$callback = array($model, $run[2]);
-				$arguments = array($schedule);
-			}
-			if (empty($callback)) 
-			{
-				Mage::throwException(Mage::helper('cronmanager')->__('No callbacks found'));
-			}
-		
-			$schedule
-			->setExecutedAt(strftime('%Y-%m-%d %H:%M:%S', time()))
-			->save();
-	
-			call_user_func_array($callback, $arguments);
-	
-			$schedule
-			->setStatus(Mage_Cron_Model_Schedule::STATUS_SUCCESS)
-			->setFinishedAt(strftime('%Y-%m-%d %H:%M:%S', time()));
-	
+			$schedule = $helper->dispatchCron($scheduleId, $schedule);
 		} 
 		catch (Exception $e) 
 		{
-			$schedule->setStatus($errorStatus)
+			$schedule->setStatus(Mage_Cron_Model_Schedule::STATUS_ERROR)
 			->setMessages($e->__toString());
+			$schedule->save();
 			$this->_redirect("*/*/edit/id/$scheduleId");
 			Mage::log($e->getMessage(),null,'cronmanager.log');
 			$this->_getSession()->addError("There was an error dispatching this cron job.");
@@ -188,77 +171,22 @@ class Ethan_Cronmanager_Adminhtml_Cronmanager_IndexController extends Mage_Admin
 			$this->_getSession()->addError($this->__('Please select product(s).'));
 		} else {
 			if (!empty($cronJobIds)) {
-				try {
-					foreach ($cronJobIds as $cronJobId) {
-						$cronJob = Mage::getSingleton('cron/schedule')->load($cronJobId);
-						$cronJob->delete();
-					}
-					$this->_getSession()->addSuccess(
-							$this->__('Total of %d record(s) have been deleted.', count($cronJobIds))
-							);
-				} catch (Exception $e) {
-					$this->_getSession()->addError($e->getMessage());
-				}
-			}
-		}
-		$this->_redirect('*/*/index');
-	}
-	
-	public function massDispatchAction()
-	{
-		$cronJobIds = $this->getRequest()->getParam('cronmanager');
-		if (!is_array($cronJobIds)) 
-		{
-			$this->_getSession()->addError($this->__('Please select product(s).'));
-		} 
-		else 
-		{
-			if (!empty($cronJobIds)) 
-			{
 				try 
 				{
 					foreach ($cronJobIds as $cronJobId) 
 					{
-						$schedule = Mage::getSingleton('cron/schedule')->load($cronJobId);
-						$jobsRoot = Mage::getConfig()->getNode('crontab/jobs');
-						$jobConfig = $jobsRoot ->{$schedule->getJobCode()};
-						$isAlways = false;
-						
-						$runConfig = $jobConfig->run;
-						
-						$errorStatus = Mage_Cron_Model_Schedule::STATUS_ERROR;
-						
-						if ($runConfig->model) {
-							if (!preg_match(Mage_Cron_Model_Observer::REGEX_RUN_MODEL, (string)$runConfig->model, $run))
-							{
-								Mage::throwException(Mage::helper('cronmanager')->__('Invalid model/method definition, expecting "model/class::method".'));
-							}
-							if (!($model = Mage::getModel($run[1])) || !method_exists($model, $run[2]))
-							{
-								Mage::throwException(Mage::helper('cronmanager')->__('Invalid callback: %s::%s does not exist', $run[1], $run[2]));
-							}
-							$callback = array($model, $run[2]);
-							$arguments = array($schedule);
-						}
-						if (empty($callback))
+						try 
 						{
-							Mage::throwException(Mage::helper('cronmanager')->__('No callbacks found'));
+							$cronJob = Mage::getSingleton('cron/schedule')->load($cronJobId);
+							$cronJob->delete();
 						}
-					
-						$schedule
-						->setExecutedAt(strftime('%Y-%m-%d %H:%M:%S', time()))
-						->save();
-					
-						call_user_func_array($callback, $arguments);
-					
-						$schedule
-						->setStatus(Mage_Cron_Model_Schedule::STATUS_SUCCESS)
-						->setFinishedAt(strftime('%Y-%m-%d %H:%M:%S', time()));
-						
-						$schedule->save();
+						catch (Exception $e)
+						{
+							Mage::log("There was an error processing cron job ID: " . $cronJobId."\n". $e->getMessage(), null, 'cronmanager.log');
+						}
 					}
 					$this->_getSession()->addSuccess(
-							$this->__('Total of %d record(s) have been dispatched.', count($cronJobIds))
+							$this->__('Total of %d record(s) have been deleted.', count($cronJobIds))
 							);
 				} 
 				catch (Exception $e) 
@@ -270,41 +198,59 @@ class Ethan_Cronmanager_Adminhtml_Cronmanager_IndexController extends Mage_Admin
 		$this->_redirect('*/*/index');
 	}
 	
-	// =============================================== UTILITIES =============================================== //
-	
-	private function _validateScheduledTime($timeStr)
+	public function massDispatchAction()
 	{
-		if ($timeStr === 'queue')
+		$helper = Mage::helper('cronmanager');
+		$cronJobIds = $this->getRequest()->getParam('cronmanager');
+		if (!is_array($cronJobIds)) 
 		{
-			// return the last scheduled cronjob +1 hour
-			$cronJobCollection = Mage::getModel('cron/schedule')->getCollection()
-					->addFieldToFilter('status', array('eq' => Mage_Cron_Model_Schedule::STATUS_PENDING));
-			$endOfQueueTime = $cronJobCollection->orderByScheduledAt()->getLastItem()->getScheduledAt();
-			$endOfQueueTime = !is_null($endOfQueueTime) ? $endOfQueueTime : now();
-			$dateObj = DateTime::createFromFormat('Y-m-d H:i:s', $endOfQueueTime);
-			$dateInterval = new DateInterval('P0Y0M0DT1H0M0S');
-			$dateObj->add($dateInterval);
-			return $dateObj->format('Y-m-d H:i:s');
-			
-		}
-		$timeStr = htmlentities(trim($timeStr), ENT_QUOTES, 'UTF-8');
-		$dateObj = DateTime::createFromFormat('Y-m-d H:i:s', $timeStr);
-		$nowObj = DateTime::createFromFormat('Y-m-d H:i:s', now());
-		if ($dateObj !== false) {
-			//valid time
-			$dateDiff = ($nowObj > $dateObj);
-			if ($dateDiff) 
+			$this->_getSession()->addError($this->__('Please select product(s).'));
+		} 
+		else 
+		{
+			if (!empty($cronJobIds)) 
 			{
-				$warning = "Your Cron Job was saved successfully, but it seems to be scheduled in the past.  If this was not your intention, please try again.";
-				return array($timeStr, 'warning' => $warning);
+				try 
+				{
+					$errors = 0;
+					foreach ($cronJobIds as $cronJobId) 
+					{
+						try 
+						{
+							$schedule = Mage::getSingleton('cron/schedule')->load($cronJobId);
+							$schedule = $helper->dispatchCron($cronJobId, $schedule);
+							$schedule->save();
+						}
+						catch (Exception $e)
+						{
+							$errors++;
+							$schedule->setStatus(Mage_Cron_Model_Schedule::STATUS_ERROR)
+							->setMessages($e->__toString());
+							$schedule->save();
+							Mage::log("There was an error dispatching cron job ID: " . $cronJobId."\n". $e->getMessage(), null, 'cronmanager.log');
+						}
+					}
+					if($errors < 1)
+					{
+						$this->_getSession()->addSuccess(
+								$this->__('Total of %d cron(s) have been dispatched.', count($cronJobIds))
+								);
+					}
+					else 
+					{
+						$this->_getSession()->addWarning(
+								$this->__('Total of %d cron(s) have been dispatched, but %d crons were not dispatched correctly.', count($cronJobIds), $errors)
+								);	
+					}
+					
+				} 
+				catch (Exception $e) 
+				{
+					$this->_getSession()->addError($e->getMessage());
+				}
 			}
-			return $timeStr;
 		}
-		else{
-			//invalid time
-			Mage::throwException("Invalid time format");
-		}
-		
+		$this->_redirect('*/*/index');
 	}
 	
 }
